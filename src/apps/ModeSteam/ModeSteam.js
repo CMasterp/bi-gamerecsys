8b6982d4dfd44984b90d30aab5eb609781495f44
Prince MIYIGBENA <christianmiyigbena@gmail.com>
2022-11-29 19:31:37 +0900

    Push

--- src/apps/ModeSteam/ModeSteam.js
+++ src/apps/ModeSteam/ModeSteam.js
@@ -169,10 +169,12 @@ const ModeSteam = () => {
         <ThemeProvider theme={theme}>
           <Box style={{ backgroundImage: `url(${BackgroundImage})`, backgroundSize: "cover", height: "100vh", color: "#f5f5f5", display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
             <Box style={{ marginTop: "15vh", height: "85vh", width: "100%", flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
-              <Box style={{ height: "100%", width: "95%", overflowY: 'scroll' }}>
-                <Grid container xs={6} md={12} spacing={2} columns={{ xs: 4, sm: 8, md: 12 }}>
-                {userArray.map((item) => (
-                        <Grid item xs={1} sm={4} md={4} key={item.appid}>
+              <Grid container rowSpacing={1} style={{ height: '85vh', overflowY: 'hidden' }}>
+                <Grid item xs={6} style={{ display: 'flex', overflowY: 'scroll' }}>
+                  <Box style={{ height: "100%", width: "95%" }}>
+                    <Grid container xs={6} md={12} spacing={2} columns={{ xs: 4, sm: 8, md: 12 }}>
+                        {userArray.map((item) => (
+                        <Grid item xs={2} sm={4} md={4} key={item.appid}>
                           <Card sx={{ maxWidth: 345 }}>
                             <CardActionArea onClick={() => { setItemClicked(item); handleClickOpen(); }} >
                               <CardMedia
@@ -194,8 +196,9 @@ const ModeSteam = () => {
                           </Card>
                         </Grid>
                       ))}
-                </Grid>
-                <Dialog open={open} onClose={handleClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+                    </Grid>
+                  </Box>
+                  <Dialog open={open} onClose={handleClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
                     <DialogTitle id="alert-dialog-title">
                       {itemClicked.name}
                     </DialogTitle>
@@ -207,8 +210,15 @@ const ModeSteam = () => {
                     <DialogActions>
                       <Button onClick={handleClose}>Close</Button>
                     </DialogActions>
-                </Dialog>
-              </Box>
+                  </Dialog>
+              </Grid>
+              <Grid item xs={6} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} onClick={onGenerate} >
+                    {!isHovering && ( <div> <img src={GenerateButton} alt="generatebutton" style={{ height: "22%" }} /> </div> )}
+                    {isHovering && ( <div> <img src={GenerateButtonHover} alt="generatebutton hover" style={{ height: "22%" }} /> </div> )}
+                  </div>
+              </Grid>
+            </Grid>
             </Box>
           </Box>
         </ThemeProvider>
